ab6c1d32442f4529af324b6dadfe531bc5b7680d
asifaminisonline <asifaminisonline@gmail.com>
2022-12-13 14:32:53 +0530

    Added required changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import './style.css';
-import { add, deleteAllCompleted, populateList } from './crud.js';
-import ToDo from './todoList.js';
-
+import "./style.css";
+import { add, deleteAllCompleted, populateList } from "./crud.js";
+import ToDo from "./todoList.js";
+const clearButton = document.getElementById("clear-btn");
 // Window load
-const list = JSON.parse(localStorage.getItem('todoList'));
+const list = JSON.parse(localStorage.getItem("todoList"));
 if (list) {
   list.forEach((item) => new ToDo(item.description, item.complete));
 }
 
 // Add
-const addInput = document.getElementById('add-input');
-addInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
+const addInput = document.getElementById("add-input");
+addInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
     add(addInput.value);
-    addInput.value = '';
+    addInput.value = "";
     populateList();
   }
 });
 
 // Delete all completed
-const clearButton = document.getElementById('clear-btn');
-clearButton.addEventListener('click', () => {
+clearButton.addEventListener("click", () => {
   deleteAllCompleted(ToDo);
   populateList();
 });
